6e1d2e1f4a75ccb428d952e8e9ac25a0c0ad76c2
LauraCalleja <132481649+LauraCalleja@users.noreply.github.com>
2024-12-29 18:53:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,6 @@ const path = require("path");
 const app = express();
 const port = 8000;
 
-app.get("/api/poems", (req, res) => {
-    res.json(poems);
-});
-
-app.get("/api/poems/:id", (req, res) => {
-    res.json(poems[req.params.id]);
-});
 
 app.use(express.static(path.join(__dirname, "public")));
 
