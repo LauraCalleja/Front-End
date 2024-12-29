cc3b7c752deeb8b3b8ba5247665578962c731cc9
LauraCalleja <132481649+LauraCalleja@users.noreply.github.com>
2024-12-29 18:59:38 +0200

    Update poems.js

--- poems.js
+++ poems.js
@@ -1,8 +1,31 @@
-app.get("/api/poems", (req, res) => {
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+let poems = [];
+
+router.get("/", (req, res) => {
     res.json(poems);
 });
 
-app.get("/api/poems/:id", (req, res) => {
+router.get("/:id", (req, res) => {
     res.json(poems[req.params.id]);
 });
+
+router.post("/", (req, res) => {
+    poems.push(req.body);
+
+    fs.writeFile("./data/poems.json", JSON.stringify(poems), err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Data saved!");
+    });
+
+    res.send(req.body);
+    // console.log("Poem: " + JSON.stringify(req.body) + " added!");
+});
+
+module.exports = router;
 
